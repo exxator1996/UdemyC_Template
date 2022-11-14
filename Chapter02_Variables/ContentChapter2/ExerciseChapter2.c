d30449ef99e8af6708741c7ff8f7de1cadf8f6cf
Niklas Guntelmann <niklasguntelmann@live.de>
2022-11-14 19:32:02 +0100

    Chapter02_Variables complete

--- Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
+++ Chapter02_Variables/ContentChapter2/ExerciseChapter2.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
+    uint64_t age, days, hours, minutes, seconds;
 
+    printf("Enter your age in years: ");
+    scanf("%lu",&age);
+
+    days = age * 365;
+    hours = days * 24;
+    minutes = hours * 60;
+    seconds = minutes * 60;
+
+    printf("You are living since %lu days\n", days);
+    printf("You are living since %lu hours\n", hours);
+    printf("You are living since %lu minutes\n", minutes);
+    printf("You are living since %lu seconds\n", seconds);
 
     return 0;
 }
