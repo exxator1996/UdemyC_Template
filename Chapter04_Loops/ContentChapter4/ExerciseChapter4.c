d560a5d5ac955e0daae777ce0a5283572ca32294
Niklas Guntelmann <niklasguntelmann@live.de>
2022-11-15 21:56:15 +0100

    Chapter 4 completed

--- Chapter04_Loops/ContentChapter4/ExerciseChapter4.c
+++ Chapter04_Loops/ContentChapter4/ExerciseChapter4.c
@@ -2,6 +2,24 @@
 
 int main()
 {
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            if(j==0)
+                printf("<");
+            else if(j==5)
+                printf("\t>\n");
+            else if (i == 0)
+                printf("\t  ^\t");
+            else if (i == 5)
+                printf("\t  v\t");
+            else
+                printf("\t(%i,%i)\t",i,j);
+
+        }
+
+    }
 
     return 0;
 }
