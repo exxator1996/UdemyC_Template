c5c9686745af26645dedcb2145349a903ce89338
Niklas Guntelmann <niklasguntelmann@live.de>
2022-11-15 20:27:41 +0100

    Chapter 3 completed

--- Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
+++ Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    __uint32_t dividend;
+
+    printf("Please enter a valid unsigned integer!\n");
+    scanf("%u", &dividend);
+
+    if(dividend % 3 != 0)
+    {
+        printf("The number you entered is not div. by 3!\n");
+        printf("The remainder of the operation is: %u\n", dividend % 3);
+    }
+    else
+    {
+        printf("The number you entered is div. by 3!\n");
+    }
 
     return 0;
 }
