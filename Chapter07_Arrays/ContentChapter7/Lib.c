329fb606aad51e34cc403e63a29a1a7a89c89220
Niklas Guntelmann <niklasguntelmann@live.de>
2022-12-05 22:38:54 +0100

    Chapter 7 completed

--- Chapter07_Arrays/ContentChapter7/Lib.c
+++ Chapter07_Arrays/ContentChapter7/Lib.c
@@ -13,3 +13,29 @@ float meanArray(int *array, unsigned int length)
 
     return (float)(sum) / (float)(length);
 }
+
+int maxArray(int *array, unsigned int length)
+{
+    int max = array[0];
+
+    for (unsigned int i = 1; i < length; i++)
+    {
+        if (max < array[i])
+            max = array[i];
+    }
+
+    return max;
+}
+
+int minArray(int *array, unsigned int length)
+{
+    int min = array[0];
+
+    for (unsigned int i = 1; i < length; i++)
+    {
+        if (min > array[i])
+            min = array[i];
+    }
+
+    return min;
+}
