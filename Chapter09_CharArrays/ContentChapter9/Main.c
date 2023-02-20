ca2d19af9b66993d531d66add6d23826acfb303e
Niklas Guntelmann <niklasguntelmann@live.de>
2023-02-20 20:17:40 +0100

    Chapter 09 completed

--- Chapter09_CharArrays/ContentChapter9/Main.c
+++ Chapter09_CharArrays/ContentChapter9/Main.c
@@ -4,6 +4,36 @@
 
 char *join(char *delimiter, char **list)
 {
+    if(delimiter == NULL || list == NULL)
+        return NULL;
+
+
+    size_t joinStringSize = 0;
+    char *joinedString = NULL;
+
+    while (*list != NULL)
+    {
+        joinStringSize += strlen(*list) + 1;
+
+        if (joinedString == NULL)
+        {
+            joinedString = (char *)malloc(joinStringSize * sizeof(char));
+            joinedString[0] = '\0';
+        }
+
+        joinedString = (char *)realloc(joinedString, joinStringSize * sizeof(char));
+        strcat(joinedString, *list);
+
+        if(*(list+1) != NULL)
+        {
+            joinStringSize += strlen(delimiter) + 1;
+            joinedString = (char *)realloc(joinedString, joinStringSize * sizeof(char));
+            strcat(joinedString, delimiter);
+        }
+        list++;
+    }
+
+    return joinedString;
 }
 
 int main()
