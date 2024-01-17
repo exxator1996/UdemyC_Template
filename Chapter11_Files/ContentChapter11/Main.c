7994fb9d5bfd206bb81521b1811b6b8f5a12e9cd
Niklas Guntelmann <niklasguntelmann@live.de>
2024-01-17 15:13:37 +0100

    Finished

--- Chapter11_Files/ContentChapter11/Main.c
+++ Chapter11_Files/ContentChapter11/Main.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 char PROJECT_DIR[] = "/mnt/c/Users/niklas/Documents/CLernen/UdemyC_Template/";
-char text[10][100];
 
 int main()
 {
     int i = 0;
-    char output_filepath[100] = {'\0'};
+    char output_filepath[200] = {'\0'};
+    char text[10][100] = {'\0'};
     strncpy(output_filepath, PROJECT_DIR, 100);
-    strncat(output_filepath, "Chapter11_Files/ContentChapter11/Exercise/InputData.txt", 60);
+    strncat(output_filepath, "Chapter11_Files/ContentChapter11/InputData.txt", 60);
 
     FILE *fp = fopen(output_filepath,"r");
     if (fp == NULL)
@@ -18,10 +18,16 @@ int main()
         return 1;
     }
 
-    while (fgets(text[i],100,fp) != NULL)
+    while ((fgets(text[i],100,fp) != NULL) && (i<10))
     {
         i++;
     }
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%s\n",text[i]);
+    }
+
 
 
 
