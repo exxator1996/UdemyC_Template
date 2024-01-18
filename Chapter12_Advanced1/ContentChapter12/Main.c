c7f07dd30f60980fd4fa8b98b9b45900cf78a58b
Niklas Guntelmann <niklasguntelmann@live.de>
2024-01-18 22:35:48 +0100

    Erster Test

--- Chapter12_Advanced1/ContentChapter12/Main.c
+++ Chapter12_Advanced1/ContentChapter12/Main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+
+#ifndef NDEBUG
+#define DEBUG
+#endif
 
 int comp(const void *elem1, const void *elem2)
 {
@@ -17,4 +22,56 @@ int comp(const void *elem1, const void *elem2)
 
 int main(int argc, char *argv[])
 {
+    #ifdef DEBUG
+        char input_filename[] = "Data.txt";
+        int inputLength = 34;
+    #endif
+    #ifdef NDEBUG
+        if (argc < 3)
+        {
+            printf("Argumente\n");
+            return -1;
+        }
+        char *input_filename = argv[1];
+        int inputLength = atoi(argv[2]);
+    #endif
+    char input_filepath[200] = "/mnt/c/Users/niklas/Documents/CLernen/UdemyC_Template/Chapter12_Advanced1/ContentChapter12/";
+
+    strncat(input_filepath, input_filename, 50);
+    int input_values[inputLength];
+
+    char line[20];
+
+    FILE *fp = fopen(input_filename,"r");
+    if (fp == NULL)
+    {
+        printf("file input open failed\n");
+        return -1;
+    }
+
+    for (int valueIdx = 0; valueIdx < inputLength; valueIdx++)
+    {
+        fgets(line,19,fp);
+        input_values[valueIdx] = atoi(line);
+    }
+
+    fclose(fp);
+
+    qsort(&input_values,(long unsigned int)inputLength,sizeof(int),comp);
+
+    fp = fopen(input_filename,"w");
+    if (fp == NULL)
+    {
+        printf("file output open failed\n");
+        return -1;
+    }
+    for (int valueIdx = 0; valueIdx < inputLength; valueIdx++)
+    {
+        fprintf(fp,"%i\n",input_values[valueIdx]);
+    }
+
+    fclose(fp);
+
+
+
 }
