15ebe0e6a8a8727a8358c1e5500e6f1c0abd00fa
Niklas Guntelmann <niklasguntelmann@live.de>
2024-03-23 13:53:30 +0100

    ExcersiseChapter3

--- Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
+++ Chapter03_ControlFlow/ContentChapter3/ExerciseChapter3.c
@@ -2,20 +2,23 @@
 
 int main()
 {
-    __uint32_t dividend;
+    unsigned int dividend, rest;
 
     printf("Please enter a valid unsigned integer!\n");
     scanf("%u", &dividend);
 
-    if(dividend % 3 != 0)
+    rest = dividend % 3;
+
+    if (rest == 0)
     {
-        printf("The number you entered is not div. by 3!\n");
-        printf("The remainder of the operation is: %u\n", dividend % 3);
+        printf("The number you entered is divisible by 3!\n");
     }
     else
     {
-        printf("The number you entered is div. by 3!\n");
+        printf("The number you entered is not divisible by 3!\n");
+        printf("The remainder of the operation is: %d\n", rest);
     }
+
 
     return 0;
 }
