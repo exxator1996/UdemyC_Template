74f55bb1ef3e861231b99d2e55b7b2a21ef2d385
Niklas Guntelmann <niklasguntelmann@live.de>
2024-03-31 15:29:16 +0200

    Completed chapter 16

--- Chapter16_SimpleCSVReader/CsvReader.h
+++ Chapter16_SimpleCSVReader/CsvReader.h
@@ -3,8 +3,16 @@
 
 /* INCLUDES */
 
+#include "Record.h"
+#include <stddef.h>
 
 /* DECLARTIONS */
+
+size_t line_count(const char *const file_path);
+
+void read_simple_csv(const char *const file_path, records_t *const records);
+
+void write_simple_csv(const char *const file_path, const records_t *const records);
 
 
 #endif /* CSV_READER_H */
